24e9e3c716aed702f8b3c16971e30761ee8f4a26
VictorBitt15 <victor.bmv15@gmail.com>
2022-08-09 18:41:34 -0300

    Migrations e database

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers
 {
@@ -11,31 +12,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento(){
-                EventoId=1,
-                Tema = "Angular",
-                Local = "Belo Horizonte",
-                QtdPessoas=250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL= "foto.png"
-            }
-        };
-        public EventoController()
+        private readonly DataContext context;
+        public EventoController(DataContext context)
         {
+            this.context = context;
             
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
