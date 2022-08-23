88d80bfdeadff10fce40cc4e3031dc3b79bc9b65
VictorBitt15 <victor.bmv15@gmail.com>
2022-08-23 15:05:59 -0300

    Navbar e Formulario

--- Front/ProEventosApp/src/app/eventos/eventos.component.ts
+++ Front/ProEventosApp/src/app/eventos/eventos.component.ts
@@ -8,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos : any;
+  public eventos : any =[];
+  public eventosFiltrados: any =[];
+  widthImg = 50;
+  marginImg = 2;
+  isCollapsedImg = false;
+  private _filtroLista = "";
+
+  public get filtroLista():string{
+    return this._filtroLista;
+  }
+  public set filtroLista(value:string){
+    this._filtroLista=value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+  filtrarEventos(filtrarPor: string ): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+       evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -17,7 +38,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void{
       this.http.get('https://localhost:5001/api/eventos').subscribe(
-        response => this.eventos = response,
+        response => {
+          this.eventos = response;
+          this.eventosFiltrados = response;
+          },
         error => console.log(error)
       );
   }
