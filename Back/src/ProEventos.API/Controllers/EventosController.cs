cf8f2317276666a071236c0dfcb1cf07eccae968
VictorBitt15 <victor.bmv15@gmail.com>
2022-09-06 17:01:49 -0300

    Organizando Diretorios e Cruds feitos

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ProEventos.API.Data;
+using ProEventos.Application.Contratos;
+using ProEventos.Domain;
+using ProEventos.Persistence;
 
 namespace ProEventos.API.Controllers
 {
@@ -12,42 +15,116 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventosController : ControllerBase
     {
-        private readonly DataContext context;
-        public EventosController(DataContext context)
+        
+        private readonly IEventoService eventoService;
+
+        public EventosController(IEventoService eventoService)
         {
-            this.context = context;
+            this.eventoService = eventoService;
             
         }
 
         [HttpGet]
-        public IEnumerable<Evento> Get()
+        //IActionResult retorna codigos http, 201, 400, 500
+        public async Task<IActionResult> Get()
         {
-            return context.Eventos;
+            try
+            {
+                 var eventos = await eventoService.GetAllEventosAsync(true);
+                 if(eventos == null) return NotFound("Nenhum evento encontrado");
+
+                 return Ok(eventos);
+            }
+            catch (Exception e)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,$"Erro ao tentar recuperar eventos. Erro: {e.Message}");
+            }
         }
 
         [HttpGet("{id}")]
-        public Evento GetById(int id)
+        public async Task<IActionResult> GetById(int id)
         {
-            return context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+            try
+            {
+                 var evento = await eventoService.GetEventoByIdAsync(id,true);
+                 if(evento == null) return NotFound("Nenhum evento encontrado");
+
+                 return Ok(evento);
+            }
+            catch (Exception e)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,$"Erro ao tentar recuperar eventos. Erro: {e.Message}");
+            }
+        }
+        [HttpGet("{tema}/tema")]
+        public async Task<IActionResult> GetByTema(string tema)
+        {
+            try
+            {
+                 var eventos = await eventoService.GetAllEventosByTemaAsync(tema,true);
+                 if(eventos== null) return NotFound("Eventos por tema não encontrados.");
+
+                 return Ok(eventos);
+            }
+            catch (Exception e)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,$"Erro ao tentar recuperar eventos. Erro: {e.Message}");
+            }
         }
 
         [HttpPost]
-        public string Post()
+        public async Task<IActionResult> Post(Evento model)
         {
-            return "Exemplo de post";
+            try
+            {
+                 var evento = await eventoService.AddEventos(model);
+                 if(evento == null) return BadRequest("Erro ao tentar adicionar evento");
+
+                 return Ok(evento);
+            }
+            catch (Exception e)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,$"Erro ao tentar adicionar evento. Erro: {e.Message}");
+            }
         }
 
         [HttpPut("{id}")]
-        public string Put(int id)
+        public async Task<IActionResult> Put(int id, Evento model)
         {
-            return $"Exemplo de put com id = {id}";
+            try
+            {
+                 var evento = await eventoService.UpdateEvento(id, model);
+                 if(evento== null) return BadRequest("Erro ao tentar adicionar evento");
+
+                 return Ok(evento);
+            }
+            catch (Exception e)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,$"Erro ao tentar atualizar evento. Erro: {e.Message}");
+            }
         }
 
         
         [HttpDelete("{id}")]
-        public string Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            return $"Exemplo de delete com id = {id}";
+            try
+            {
+                 if(await eventoService.DeleteEvento(id))
+                    return Ok("Deletado");
+                 else
+                    return BadRequest("Evento não deletado");
+            }
+            catch (Exception e)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError,$"Erro ao tentar deletar evento. Erro: {e.Message}");
+            }
         }
     }
 }
---
+++ Back/src/ProEventos.Persistence/EventoPersist.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Persistence.Contratos;
+using ProEventos.Persistence.Contextos;
+
+namespace ProEventos.Persistence
+{
+    public class EventoPersist : IEventoPersist
+    {
+        private readonly ProEventosContext _context;
+        public EventoPersist(ProEventosContext context)
+        {
+            this._context = context;
+            //_context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking; Se for necessário segurar o objeto, podemos resolver o problema colocando em cada método a funcção AsNoTracking() na query estabelecida
+            
+        }
+        
+
+
+        public async Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes = false)
+        {
+            IQueryable<Evento> query = _context.Eventos.Include(e => e.Lote).
+           Include(e => e.RedesSociais);
+
+           if(includePalestrantes){
+            query = query.Include(e => e.PalestranteEventos).
+            ThenInclude(pe=> pe.Palestrante);
+           }
+
+           query = query.OrderBy(e=>e.Id)
+           .Where(e => e.Id == eventoId).AsNoTracking();
+
+           return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false)
+        {
+           IQueryable<Evento> query = _context.Eventos.Include(e => e.Lote).
+           Include(e => e.RedesSociais);
+
+           if(includePalestrantes){
+            query = query.Include(e => e.PalestranteEventos).
+            ThenInclude(pe=> pe.Palestrante);
+           }
+
+           query = query.OrderBy(e=>e.Id).AsNoTracking();
+
+           return await query.ToArrayAsync();
+        }
+
+        public async Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes= false)
+        {
+            IQueryable<Evento> query = _context.Eventos.Include(e => e.Lote).
+           Include(e => e.RedesSociais);
+
+           if(includePalestrantes){
+            query = query.Include(e => e.PalestranteEventos).
+            ThenInclude(pe=> pe.Palestrante);
+           }
+
+           query = query.OrderBy(e=>e.Id).AsNoTracking()
+           .Where(e => e.Tema.ToLower().Contains(tema.ToLower()));
+
+           return await query.ToArrayAsync();
+        }
+       
+    }
+}
